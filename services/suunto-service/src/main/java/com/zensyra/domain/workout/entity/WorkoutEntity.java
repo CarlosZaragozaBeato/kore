ae43db60558b619bf0cdc57c7272b917770590fa
Carlos Zaragoza Beato <carloszaragozabeato15@gmail.com>
2026-09-12 16:37:22 +0200

    up

--- services/suunto-service/src/main/java/com/zensyra/domain/workout/entity/WorkoutEntity.java
+++ services/suunto-service/src/main/java/com/zensyra/domain/workout/entity/WorkoutEntity.java
@@ -1,7 +1,18 @@
 package com.zensyra.domain.workout.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import com.zensyra.domain.workout.model.FitnessMetrics;
+import com.zensyra.domain.workout.model.Gear;
+import com.zensyra.domain.workout.model.IntensityZones;
+import com.zensyra.domain.workout.model.Position;
+import com.zensyra.domain.workout.model.TrainingLoad;
+import com.zensyra.domain.workout.model.Weather;
+import com.zensyra.domain.workout.model.WorkoutSummaryMetrics;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Column;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -53,6 +64,105 @@ public class WorkoutEntity extends PanacheEntityBase {
 
     @Column(name = "step_count")
     public Integer stepCount;
+
+    @Column(name = "workout_id")
+    public Integer workoutId;
+
+    @Column(name = "stop_time")
+    public OffsetDateTime stopTime;
+
+    @Column(name = "last_modified")
+    public Long lastModified;
+
+    @Column(name = "avg_pace")
+    public Double avgPace;
+
+    @Column(name = "avg_speed")
+    public Double avgSpeed;
+
+    @Column(name = "max_speed")
+    public Double maxSpeed;
+
+    @Column(name = "avg_power")
+    public Double avgPower;
+
+    @Column(name = "max_power")
+    public Double maxPower;
+
+    @Column(name = "max_altitude")
+    public Double maxAltitude;
+
+    @Column(name = "min_altitude")
+    public Double minAltitude;
+
+    @Column(name = "recovery_time")
+    public Double recoveryTime;
+
+    @Column(name = "cumulative_recovery_time")
+    public Double cumulativeRecoveryTime;
+
+    @Column(name = "time_offset_minutes")
+    public Integer timeOffsetInMinutes;
+
+    @Column(name = "estimated_floors_climbed")
+    public Integer estimatedFloorsClimbed;
+
+    @Column(name = "edited")
+    public Boolean edited;
+
+    @Column(name = "manually_added")
+    public Boolean manuallyAdded;
+
+    @Column(name = "comment_count")
+    public Integer commentCount;
+
+    @Column(name = "picture_count")
+    public Integer pictureCount;
+
+    @Column(name = "view_count")
+    public Integer viewCount;
+
+    @Column(name = "hr_max")
+    public Double hrMax;
+
+    @Column(name = "user_max_heart_rate")
+    public Double userMaxHeartRate;
+
+    @Embedded
+    public TrainingLoad trainingLoad;
+
+    @Embedded
+    public FitnessMetrics fitnessMetrics;
+
+    @Embedded
+    public Gear gear;
+
+    @Embedded
+    public Weather weather;
+
+    @Embedded
+    public WorkoutSummaryMetrics summaryMetrics;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "latitude", column = @Column(name = "start_latitude")),
+            @AttributeOverride(name = "longitude", column = @Column(name = "start_longitude"))
+    })
+    public Position startPosition;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "latitude", column = @Column(name = "stop_latitude")),
+            @AttributeOverride(name = "longitude", column = @Column(name = "stop_longitude"))
+    })
+    public Position stopPosition;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "latitude", column = @Column(name = "center_latitude")),
+            @AttributeOverride(name = "longitude", column = @Column(name = "center_longitude"))
+    })
+    public Position centerPosition;
 
     public static void updateOrInsert(SuuntoWorkoutDto dto, String rawPayload) {
         WorkoutEntity entity = findById(dto.workoutKey());
